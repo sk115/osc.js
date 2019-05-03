7d352a8ba994ed272e2874c40d90e1da52410f21
voodoohop <voodoo@voodoohop.com>
2019-05-03 09:25:45 +0200

    added types and removed serial port

--- src/platforms/osc-node.js
+++ src/platforms/osc-node.js
@@ -29,7 +29,7 @@
     };
 
     var dgram = require("dgram"),
-        SerialPort = require("serialport"),
+        // SerialPort = require("serialport"),
         net = require("net"),
         WebSocket = require("ws"),
         modules = [
@@ -43,75 +43,75 @@
      * Serial *
      **********/
 
-    osc.SerialPort = function (options) {
-        this.on("open", this.listen.bind(this));
-        osc.SLIPPort.call(this, options);
-        this.options.bitrate = this.options.bitrate || 9600;
+    // osc.SerialPort = function (options) {
+    //     this.on("open", this.listen.bind(this));
+    //     osc.SLIPPort.call(this, options);
+    //     this.options.bitrate = this.options.bitrate || 9600;
 
-        this.serialPort = options.serialPort;
-        if (this.serialPort) {
-            this.emit("open", this.serialPort);
-        }
-    };
+    //     this.serialPort = options.serialPort;
+    //     if (this.serialPort) {
+    //         this.emit("open", this.serialPort);
+    //     }
+    // };
 
-    var p = osc.SerialPort.prototype = Object.create(osc.SLIPPort.prototype);
-    p.constructor = osc.SerialPort;
+    // var p = osc.SerialPort.prototype = Object.create(osc.SLIPPort.prototype);
+    // p.constructor = osc.SerialPort;
 
-    p.open = function () {
-        if (this.serialPort) {
-            // If we already have a serial port, close it and open a new one.
-            this.once("close", this.open.bind(this));
-            this.close();
-            return;
-        }
+    // p.open = function () {
+    //     if (this.serialPort) {
+    //         // If we already have a serial port, close it and open a new one.
+    //         this.once("close", this.open.bind(this));
+    //         this.close();
+    //         return;
+    //     }
 
-        var that = this;
+    //     var that = this;
 
-        this.serialPort = new SerialPort(this.options.devicePath, {
-            baudRate: this.options.bitrate,
-            autoOpen: false
-        });
+    //     this.serialPort = new SerialPort(this.options.devicePath, {
+    //         baudRate: this.options.bitrate,
+    //         autoOpen: false
+    //     });
 
-        this.serialPort.on("error", function (err) {
-            that.emit("error", err);
-        });
+    //     this.serialPort.on("error", function (err) {
+    //         that.emit("error", err);
+    //     });
 
-        this.serialPort.on("open", function () {
-            that.emit("open", that.serialPort);
-        });
+    //     this.serialPort.on("open", function () {
+    //         that.emit("open", that.serialPort);
+    //     });
 
-        this.serialPort.open();
-    };
+    //     this.serialPort.open();
+    // };
 
-    p.listen = function () {
-        var that = this;
+    // p.listen = function () {
+    //     var that = this;
 
-        this.serialPort.on("data", function (data) {
-            that.emit("data", data, undefined);
-        });
+    //     this.serialPort.on("data", function (data) {
+    //         that.emit("data", data, undefined);
+    //     });
 
-        this.serialPort.on("close", function () {
-            that.emit("close");
-        });
+    //     this.serialPort.on("close", function () {
+    //         that.emit("close");
+    //     });
 
-        that.emit("ready");
-    };
+    //     that.emit("ready");
+    // };
 
-    p.sendRaw = function (encoded) {
-        if (!this.serialPort || !this.serialPort.isOpen) {
-            osc.fireClosedPortSendError(this);
-            return;
-        }
+    // p.sendRaw = function (encoded) {
+    //     if (!this.serialPort || !this.serialPort.isOpen) {
+    //         osc.fireClosedPortSendError(this);
+    //         return;
+    //     }
 
-        var that = this;
-        this.serialPort.write(encoded);
-    };
+    //     var that = this;
+    //     this.serialPort.write(encoded);
+    // };
 
-    p.close = function () {
-        if (this.serialPort) {
-            this.serialPort.close();
-        }
-    };
+    // p.close = function () {
+    //     if (this.serialPort) {
+    //         this.serialPort.close();
+    //     }
+    // };
 
 
     /*******
